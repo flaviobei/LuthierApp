c05aa2dda7bee8df48d659cee31402968c77e3c3
flaviobei <flaviobei@gmail.com>
2026-05-15 21:08:37 -0300

    feat: update PWA manifest icons and optimize asset configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     vue(),
     VitePWA({
       registerType: "autoUpdate", // Faz a app atualizar sozinha no telemóvel quando você mudar o código
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
       manifest: {
         name: "Gestão Luthieria",
         short_name: "Luthieria",
@@ -18,12 +18,12 @@ export default defineConfig({
         orientation: "portrait",
         icons: [
           {
-            src: "/icon-192x192.png",
+            src: "/android-chrome-192x192.png",
             sizes: "192x192",
             type: "image/png",
           },
           {
-            src: "/icon-512x512.png",
+            src: "/android-chrome-512x512.png",
             sizes: "512x512",
             type: "image/png",
             purpose: "any maskable",
